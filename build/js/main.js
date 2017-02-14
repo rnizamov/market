fe85770e00caa3e574d46cb12c0518c1fb1bf57a
Nizamov Rinat <rnizamov@ovapress.ru>
2017-02-14 14:50:00 +0300

    done slick slider 90%

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,7 @@
 $('.slider').slick({
         variableWidth: true,
         arrows: false,
-        slidesToShow:1,
+        slidesToShow:3,
         slidesToScroll:1,
         infinite: true,
     }
